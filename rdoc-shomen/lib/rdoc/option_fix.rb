af2cec2b9bb5c1d2b5826c65475e9822aee4fb8b
7rans <transfire@gmail.com>
2010-09-27 13:52:18 -0400

    separate option fix into own file

---
+++ rdoc-shomen/lib/rdoc/option_fix.rb
@@ -0,0 +1,41 @@
+# This patch to RDoc::Option makes it possible use different
+# templates by registering them like this:
+#
+#   RDoc.generator_option('shomen') do
+#     require 'rdoc/generator/shomen'
+#     RDoc::Generator::Shomen
+#   end
+#
+# The block simply needs to return the custom generator class.
+
+unless defined?($FIXED_RDOC_OPTIONS)
+  $FIXED_RDOC_OPTIONS = true
+
+  #
+  def RDoc.generator_option(name, &blk)
+    @_generators ||= {}
+    if blk
+      @_generators[name.to_s] = blk
+    else
+      @_generators[name.to_s]
+    end
+  end
+
+  #
+  class RDoc::Options
+    alias_method :rdoc_parse, :parse
+
+    def parse(argv)
+      rdoc_parse(argv)
+      begin
+        if blk = RDoc.generator_option(@template)
+          @generator = blk.call
+        end
+      rescue Exception => err
+        $stderr.puts "problem loading template #{@template} --falling back to default"
+      end
+    end
+  end
+
+end
+
