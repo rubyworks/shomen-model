e6033ead8544f9f706a68766ee6d7e616cc4d224
7rans <transfire@gmail.com>
2012-04-20 10:14:44 -0400

    Move eveything up to shomen-model directory.

---
+++ lib/shomen-model.rb
@@ -0,0 +1,31 @@
+# Shomen is an intermediary documentation model designed for documenting
+# object-oriented programming languages, particularly Ruby. The specification
+# is a flat mapping, without internal referencing, suitable for storage in both
+# YAML and JSON formats.
+#
+module Shomen
+end
+
+if RUBY_VERSION > '1.9'
+  require_relative 'shomen-model/generator'
+  require_relative 'shomen-model/abstract'
+  require_relative 'shomen-model/document'
+  require_relative 'shomen-model/script'
+  require_relative 'shomen-model/module'
+  require_relative 'shomen-model/class'
+  require_relative 'shomen-model/method'
+  require_relative 'shomen-model/attribute'
+# require_relative 'shomen-model/function'
+  require_relative 'shomen-model/constant'
+else
+  require 'shomen-model/generator'
+  require 'shomen-model/abstract'
+  require 'shomen-model/document'
+  require 'shomen-model/script'
+  require 'shomen-model/module'
+  require 'shomen-model/class'
+  require 'shomen-model/method'
+  require 'shomen-model/attribute'
+# require 'shomen-model/function'
+  require 'shomen-model/constant'
+end
